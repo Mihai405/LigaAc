6b8f83ca2ef7440e973c9fcedea56455129768b3
Mihai <you@example.com>
2021-05-28 10:12:02 +0300

    Home page

--- liga-react/src/App.js
+++ liga-react/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AuthContextProvider } from './Auth/AuthContext';
 import { Login } from './Auth/Login';
-import { Register } from './Auth/Register';
+import { Register } from './Auth/Register';import { HomePage } from './HomePage/HomePage';
 import { MovieList , MovieDetail} from './Movie';
 import  NavBar  from "./Nav/NavBar";
 
@@ -11,7 +11,7 @@ export function App() {
     <BrowserRouter>
       <NavBar />
       <Switch>
-        <Route exact path="/" component={() => <h1>Home Page</h1>} />
+        <Route exact path="/" component={HomePage} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <Route exact path="/movies" component={MovieList} />
---
+++ liga-react/src/HomePage/HomePage.js
@@ -0,0 +1,8 @@
+import styles from './HomePage.module.css';
+
+
+export function HomePage(){
+    return (
+        <h1 className={styles['home-page']}>OMDB Home Page</h1>
+    )
+}
